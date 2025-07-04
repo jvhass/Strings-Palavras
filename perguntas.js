5f24ab5604eb67a1892b3edd3df0d6574a8229d5
João hass <joao.salomao.hass@escola.pr.gov.br>
2025-07-04 16:47:32 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,9 @@
-
+criaCartao('Geografia', 'Qual é o maior oceano do planeta?', '🌊 Oceano Pacífico');
+criaCartao('Artes', 'Quem pintou a obra “Mona Lisa”?', '🎨 Leonardo da Vinci');
+criaCartao('Geografia', 'Qual é o país mais populoso do mundo?', '🌍 China');
+criaCartao('Ciências', 'Quantos planetas existem no Sistema Solar?', '🪐 8 planetas');
+criaCartao('Química', 'Qual é o elemento químico representado pelo símbolo ‘O’?','⚗️ Oxigênio');
+criaCartao('Geografia', 'Em que continente fica o Deserto do Saara?', '🏜️ África');
+criaCartao('História', 'Quem foi o primeiro homem a pisar na Lua?', '🚀 Neil Armstrong');
+criaCartao('Geografia', 'Qual é a capital do Japão?', '🌆 Tóquio');
+criaCartao('História do Brasil', 'Em que ano o Brasil foi descoberto pelos portugueses?', '🇧🇷 1500');

5270909df56ced2d5ade44a3b3c8dc2184066466
João hass <joao.salomao.hass@escola.pr.gov.br>
2025-07-04 16:42:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+function criaCartao(titulo, pergunta, resposta) {
+    const container = document.getElementById('container');
+    const cartao = document.createElement('article');
+    cartao.classList.add('cartao');
 
+    const conteudo = document.createElement('div');
+    conteudo.classList.add('cartao__conteudo');
+
+    const front = document.createElement('div');
+    front.classList.add('cartao__conteudo__pergunta');
+
+    const tituloElem = document.createElement('h3');
+    tituloElem.textContent = titulo;
+
+    const perguntaElem = document.createElement('p');
+    perguntaElem.textContent = pergunta;
+
+    front.appendChild(tituloElem);
+    front.appendChild(perguntaElem);
